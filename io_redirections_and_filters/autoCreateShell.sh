8f6ffff846f9ba6cc2f41b5e8c77ce53309348d7
Roxxion <hiojam2016@gmail.com>
2023-02-13 04:20:28 -0800

    Task 0

---
+++ io_redirections_and_filters/autoCreateShell.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Introduce el nombre del archivo a crear: "
+echo ""
+read fileName
+echo "Introduce el código: "
+echo ""
+read code
+echo "#!/bin/bash" >> $fileName
+echo $code >> $fileName
+chmod 777 $fileName
+
+echo "Quieres crear un commit y push con este archivo? 0 = No, 1 = Si"
+echo ""
+read option
+if [[ $option -eq 1 ]]; then
+   echo "Introduce el mensaje del commit:";
+   echo; 
+   read commitMsg;
+   git add .;
+   git commit -m "$commitMsg";
+   git push;
+   exit;
+fi
